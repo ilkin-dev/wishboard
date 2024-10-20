c1d3d0e7ef254dbfbe6553b834323682933e2fd8
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-20 08:17:41 -0400

    Update: App files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import React from 'react';
 import GoogleAuthProviderWrapper from './context/GoogleAuthProvider';
+import Modal from './components/Modal/Modal';
+import { ModalProvider } from './context/ModalContext';
 
 function App() {
+
   return (
     <div className="App">
       <GoogleAuthProviderWrapper>
-        <HomePage />
+        <ModalProvider>
+          <HomePage />
+          <Modal />
+        </ModalProvider>
       </GoogleAuthProviderWrapper>
+
     </div>
   );
 }
