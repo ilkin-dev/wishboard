7d5ce4de0fa18b55f7e0ee38dc35dc291ba57556
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-20 09:34:48 -0400

    Update HomePage.js

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderSection from '../../components/Header/HeaderSection';
 import HeroSection from '../../components/Hero/HeroSection';
+import AboutSection from '../../components/AboutSection/AboutSection';
 
 const Homepage = () => {
 
@@ -8,6 +9,7 @@ const Homepage = () => {
         <>
             <HeaderSection />
             <HeroSection />
+            <AboutSection />
         </>
     );
 };
