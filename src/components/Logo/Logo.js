690b4f60b3b5e17f222f5ed4eb2e9c0b685443bf
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-20 08:13:39 -0400

    Add: Logo reusable component
    For now, I use text. Later, it can be replaced with image. style is already ready.

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Logo.scss';
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <a href="/" className="logo__link">
+                <span className="logo__text">WishBoard</span>
+            </a>
+        </div>
+    );
+};
+
+export default Logo;
