12c324757acc19dc031f58756d91081ef0c554d2
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-22 06:12:30 -0400

    Update HeaderSection.js

--- src/components/Header/HeaderSection.js
+++ src/components/Header/HeaderSection.js
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './HeaderSection.scss';
 import Logo from '../Logo/Logo';
 import NavLinks from '../NavLinks/NavLinks';
 import CTAButtons from '../CTAButtons/CTAButtons';
-import { jwtDecode } from 'jwt-decode';
 
 const HeaderSection = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [userProfile, setUserProfile] = useState(null);
-
-    useEffect(() => {
-        const jwtToken = localStorage.getItem('jwtToken');
-        if (jwtToken) {
-            try {
-                const decodedToken = jwtDecode(jwtToken);
-                setUserProfile({
-                    name: decodedToken.name,
-                    email: decodedToken.email,
-                    profilePicture: decodedToken.picture,
-                });
-            } catch (error) {
-                console.error('Invalid token:', error);
-            }
-        }
-    }, []);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -36,7 +18,7 @@ const HeaderSection = () => {
                 <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
                     <NavLinks />
                 </nav>
-                <CTAButtons userProfile={userProfile} />
+                <CTAButtons />
                 <button className="header__burger" onClick={toggleMenu}>
                     <span className="header__burger-line"></span>
                     <span className="header__burger-line"></span>
