81b87b7e25cb45a6690c682e6b59720996e78f67
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-22 07:07:13 -0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
-// Get token from localStorage
-const token = localStorage.getItem('jwtToken');
-
 // Create an axios instance
 const apiClient = axios.create({
     baseURL: 'http://localhost:8080/api',
-    headers: {
-        Authorization: token ? `Bearer ${token}` : '', // Attach token if it exists
-        'Content-Type': 'application/json'
+});
+
+// Add a request interceptor to include the token in each request
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('jwtToken');  // Get token from localStorage
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;  // Add token to request headers
     }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
 });
 
 export default apiClient;
