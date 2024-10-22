ee442f8519b4662496dbfa1791184be35ee0c9f6
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-22 18:50:20 -0400

    Add: GoalBox

--- src/components/GoalBox/GoalBox.js
+++ src/components/GoalBox/GoalBox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './GoalBox.scss';
 
-const GoalBox = ({ goal, day }) => {
+const GoalBox = ({ day, goal, isPassedDay, isCurrentDay }) => {
+    // Apply styles based on whether the day is passed or the current day
+    const dayStyle = isCurrentDay
+        ? 'goal-box--current'  // Secondary color for today
+        : isPassedDay
+            ? 'goal-box--passed'   // Faded color for passed days
+            : '';
+
     return (
-        <div className="goal-box" onClick={() => console.log(`Goal for Day ${day}`)}>
-            <div className="goal-box__content">
-                <span>{goal.title || `Day ${day}`}</span>
+        <div className={`goal-box ${dayStyle}`}>
+            <div className="goal-box__day-number">
+                {day.getDate()}  {/* Show just the day of the month */}
             </div>
         </div>
     );
