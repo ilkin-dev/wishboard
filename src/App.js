87f41b0df202d5297dc55d33a79775c7061b70ee
Ilkin Mammadzada <ilkindigo.ca@gmail.com>
2024-10-22 01:40:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
+import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import HomePage from './pages/HomePage/HomePage';
-import ExplorePage from './pages/ExplorePage/ExplorePage';
-import Modal from './components/Modal/Modal';
 import GoogleAuthProviderWrapper from './context/GoogleAuthProvider';
-import { ModalProvider } from './context/ModalContext';
+import HomePage from './pages/HomePage/HomePage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
+import ExplorePage from './pages/ExplorePage/ExplorePage';
+import { ModalProvider } from './context/ModalContext';
+import { AuthProvider } from './context/AuthContext'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <GoogleAuthProviderWrapper>
-        <ModalProvider>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/explore" element={<ExplorePage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Routes>
-          <Modal />
-        </ModalProvider>
-      </GoogleAuthProviderWrapper>
-    </Router>
+    <div className="App">
+      <Router>
+        <GoogleAuthProviderWrapper>
+          <AuthProvider>
+            <ModalProvider>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/explore" element={<ExplorePage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+              </Routes>
+            </ModalProvider>
+          </AuthProvider>
+        </GoogleAuthProviderWrapper>
+      </Router>
+    </div>
   );
 }
 
